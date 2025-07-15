b62bccf3c9ab91e60118c02f458d4af0f4a8d391
mkimball14 <maxkimball14@gmail.com>
2025-07-15 16:03:27 -0500

    Change javascript to list worksheets

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-tableau.extensions.initializeAsync().then(() => {
-    const dashboard = tableau.extensions.dashboardContent.dashboard;
-  
-    const worksheets = dashboard.worksheets;
-    const targetSheet = worksheets[0]; // Change if you want a specific worksheet
-  
-    targetSheet.getSummaryDataAsync().then((dataTable) => {
-      const dataDiv = document.getElementById("data-output");
-      dataTable.data.forEach((row) => {
-        const values = row.map(cell => cell.formattedValue).join(" | ");
-        dataDiv.innerHTML += `<p>${values}</p>`;
-      });
-    });
-  });
-  
+tableau.extensions.initializeAsync().then(function() {
+  let worksheetNames = tableau.extensions.dashboardContent.dashboard.worksheets.map(ws => ws.name);
+  document.getElementById("data-output").innerHTML = "Worksheets: <br>" + worksheetNames.join("<br>");
+}, function(err) {
+  document.getElementById("data-output").innerText = "Extension error: " + err.message;
+});
